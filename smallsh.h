2dbdc820036c2df138596f36ee342c83ee77da95
Jared Connor <jaredconnor301@gmail.com>
2022-01-24 20:32:32 -0800

    C file template structure

---
+++ smallsh.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define MAX_ARGS      		512
+#define MAX_INPUT_LENGTH    2048
